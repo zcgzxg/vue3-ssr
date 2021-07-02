e4073013b3467bc224053379f24d47245a3ac407
周瑜 <1134495285@qq.com>
2021-07-02 16:41:04 +0800

    初始化项目

---
+++ web/@types/global.d.ts
@@ -0,0 +1,18 @@
+import { IWindow } from 'ssr-types'
+import { App } from 'vue'
+import { Router } from 'vue-router'
+
+declare module '*.less'
+
+declare global {
+  interface Window {
+    __USE_SSR__?: IWindow['__USE_SSR__']
+    __INITIAL_DATA__?: IWindow['__INITIAL_DATA__']
+    STORE_CONTEXT?: IWindow['STORE_CONTEXT']
+    __VUE_APP__: App
+    __VUE_ROUTER__: Router
+    __USE_VITE__?: boolean
+  }
+  const __isBrowser__: Boolean
+}
+
