c191da35f318b2130eacbd78ded368e290609026
周瑜 <1134495285@qq.com>
2021-07-15 15:30:29 +0800

    记录页面关联的css文件

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+const cssManifest = {}
 
 /**
  * @see [config document](http://doc.ssr-fc.com/docs/api$config)
@@ -22,30 +23,61 @@ const config = {
         return args
       })
   },
-  // chainClientConfig: chain => {
-  //   chain.optimization
-  //     .splitChunks({
-  //       chunks: 'async',
-  //       minSize: 20000,
-  //       minChunks: 1,
-  //       maxAsyncRequests: 30,
-  //       maxInitialRequests: 30,
-  //       enforceSizeThreshold: 50000,
-  //       cacheGroups: {
-  //         vendors: {
-  //           test: /[\\/]node_modules[\\/]/,
-  //           priority: -10,
-  //           reuseExistingChunk: true
-  //         },
-  //         common: {
-  //           minChunks: 2,
-  //           priority: -20,
-  //           reuseExistingChunk: true
-  //         }
-  //       }
-  //     })
-  //     .end()
-  // }
+  chainClientConfig: chain => {
+    chain.optimization
+      .splitChunks({
+        chunks: 'async',
+        minSize: 20000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        name(module, chunks, cacheGroupKey) {
+          const moduleFileName = module
+            .identifier()
+            .split('/')
+            .reduceRight((item) => item)
+          const allChunksNames = chunks.map((item) => item.name)
+          const name = `${cacheGroupKey}-${allChunksNames.join('~')}`
+
+          if (
+            moduleFileName.endsWith('.css') ||
+            moduleFileName.endsWith('.less')
+          ) {
+            cssManifest[name + '.css'] = allChunksNames
+          }
+          return name
+        },
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true
+          },
+          common: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
+      })
+      .end()
+
+    chain.plugin('WriteCssManifest').use(
+      class WriteCssManifest {
+        apply(compiler) {
+          compiler.hooks.done.tapAsync(
+            'WriteCssManifest',
+            (params, callback) => {
+              console.log(cssManifest)
+              // 将cssManifest定入文件
+              callback()
+            }
+          )
+        }
+      }
+    )
+  }
 }
 
 module.exports = config
