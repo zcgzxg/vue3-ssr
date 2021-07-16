e6f3507242e07f2923d82fc53a8d11a89a19839d
tn_zhuxg <1134495285@qq.com>
2021-07-16 22:00:01 +0800

    antd图标样式问题重现

---
+++ web/pages/index/render.vue
@@ -0,0 +1,13 @@
+<template>
+  <Pagination :total="200" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+import { Pagination } from 'ant-design-vue'
+
+export default defineComponent({
+  name: 'Index',
+  components: { Pagination }
+})
+</script>
